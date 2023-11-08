3b8f5f83efd97c87f999f6d902469ba3e6a69e31
anbarasu4 <anbarasu@thetopclassentertainment.com>
2023-11-08 15:50:47 +0530

    first commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<neliveplayer_core/NELivePlayerPlugin.h>)
+#import <neliveplayer_core/NELivePlayerPlugin.h>
+#else
+@import neliveplayer_core;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [NELivePlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"NELivePlayerPlugin"]];
+}
+
+@end
